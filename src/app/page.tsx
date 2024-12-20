d9454d0f8ac3e361d22111b5d6d6d192e1527c63
harshath <harshathmkulal@gmail.com>
2024-12-20 23:14:19 +0530

    Add PageFooter component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import PageHeader from "@/components/Page-header"
+import PageFooter from "@/components/Page-footer"
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <PageHeader/>
+      <PageFooter/>
     </div>
   )
 }
