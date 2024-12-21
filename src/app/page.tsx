42061d55360f3e383dafa0c018089e0cd5e0406d
harshath <harshathmkulal@gmail.com>
2024-12-21 16:52:23 +0530

    Integrate PageHeader and PageFooter components into the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import React from "react"
-import PageHeader from "@/components/Page-header"
-import PageFooter from "@/components/Page-footer"
-import PageHome from "@/components/page-home"
+import React from "react";
+import PageHome from "@/components/page-home";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <PageHeader/>
-      <PageHome/>
-      <PageFooter/>
+      <PageHome />
     </div>
-  )
+  );
 }
