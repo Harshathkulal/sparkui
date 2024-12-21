1fd2bfa4c5ebf99a95435e433c3ce87cf6eef880
harshath <harshathmkulal@gmail.com>
2024-12-21 23:58:35 +0530

    Added main-nav

--- src/components/Page-header.tsx
+++ src/components/Page-header.tsx
@@ -1,45 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Menu } from "lucide-react";
-import Link from "next/link";
 import React from "react";
 import { ModeSwitcher } from "./mode-switcher";
+import { MainNav } from "./main-nav";
 
 const PageHeader = () => {
   return (
     <>
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-14 items-center justify-between">
           <div className="flex items-center space-x-4">
-            <Link className="flex items-center space-x-2" href="/">
-              <span className="font-bold text-lg">UdasinUI</span>
-            </Link>
-            <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
-              <Link
-                className="transition-colors hover:text-foreground/80 text-foreground/60"
-                href="/docs"
-              >
-                Docs
-              </Link>
-              <Link
-                className="transition-colors hover:text-foreground/80 text-foreground/60"
-                href="/components"
-              >
-                Components
-              </Link>
-              <Link
-                className="transition-colors hover:text-foreground/80 text-foreground/60"
-                href="/themes"
-              >
-                Themes
-              </Link>
-              <Link
-                className="transition-colors hover:text-foreground/80 text-foreground/60"
-                href="/examples"
-              >
-                Examples
-              </Link>
-            </nav>
+            <MainNav/>
           </div>
           <div className="flex items-center space-x-4">
             <div className="hidden sm:block">
---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+    name: "Udasina/ui",
+    url: "https://Udasina.ui",
+    ogImage: "https://.jpg",
+    description:
+      "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+    links: {
+      twitter: "https://twitter.com/",
+      github: "https://github.com/",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
+  
+  export const META_THEME_COLORS = {
+    light: "#ffffff",
+    dark: "#09090b",
+  }
